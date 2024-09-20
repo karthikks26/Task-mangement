7951c458511005aaa667214996bc275343f27c0c
Karthik ks <karthikks260703@gmail.com>
2024-09-20 16:47:41 +0530

    c8

--- middleware.ts
+++ middleware.ts
@@ -3,16 +3,25 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const isPublicPath = path === "/login" || path === "/signup" || path === "/";
+  const isPublicPath = path === "/login" || path === "/signup";
+  const isHomePath = path === "/";
   const token = request.cookies.get("user")?.value || "";
+
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
-  if (!isPublicPath && !token) {
+
+  if (isHomePath && token) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (!isPublicPath && !isHomePath && !token) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/login", "/signup", "/dashboard/:path*"],
+  matcher: ["/", "/login", "/signup", "/dashboard/:path*"],
 };
